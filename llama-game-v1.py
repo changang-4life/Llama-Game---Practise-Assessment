8d9c191bd79783e2af4a69eb4bd768ef16e9a0cc
changang-4life <akinboj2@middleton.school.nz>
2026-04-22 10:59:38 +1200

    Version 1

---
+++ llama-game-v1.py
@@ -0,0 +1,16 @@
+""" Llama Game
+Jade Akinbo
+v1 - Establish Game Display
+"""
+
+import pygame
+import time
+
+pygame.init() # initialises pygame so it's ready to use
+
+screen = pygame.display.set_mode((1000, 720)) # sets up the screen display
+
+time.sleep(5) # wait period for testing purposes
+
+pygame.quit()
+quit()
