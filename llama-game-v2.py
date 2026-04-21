3cbcfc175af3d02c067c49119a7e04e1eb24c7d1
changang-4life <akinboj2@middleton.school.nz>
2026-04-22 10:59:49 +1200

    Version 2

---
+++ llama-game-v2.py
@@ -0,0 +1,14 @@
+""" Llama Game
+v2 - Import Llama
+"""
+
+import pygame
+import time
+
+pygame.init() # initialises pygame so it's ready to use
+
+llama_sprite = pygame.image.load("Llama.png") # loads the llama sprite in
+screen = pygame.display.set_mode((1100, 550)) # sets up the screen display
+
+pygame.quit()
+quit()
