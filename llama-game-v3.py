b82a36b0119b8bfb761a80a8e21d1a02dc8edc17
changang-4life <akinboj2@middleton.school.nz>
2026-04-22 13:25:44 +1200

    what is this

--- llama-game-v3.py
+++ llama-game-v3.py
@@ -1,5 +1,7 @@
 """ Llama Game
-v3 - Running the Game
+v3 - Obstacles
+    > Cactus PNG
+    > Beginning of the game loop functionality (for tests)
 """
 
 import pygame
@@ -10,10 +12,20 @@ pygame.init() # initialises pygame so it's ready to use
 llama_sprite = pygame.image.load("Llama.png")
 screen = pygame.display.set_mode((1100, 550)) # sets up the screen display
 
+obstacle = pygame.image.load("cactus.png")
+
+
 def game_loop():
-    screen.blit(llama_sprite, 100, 100)
-    
-time.sleep(10) # wait period for testing purposes
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT: # when user wants to close out the window
+                running = False 
+
+        screen.blit(llama_sprite, (100, 100)) # displays the llama sprite
+        screen.blit(obstacle, (150, 150)) # displays the cactus obstacle
+
+        pygame.display.update()
 
 game_loop()
 
---
+++ llama-game-v4.py
@@ -0,0 +1,36 @@
+""" Llama Game
+v4 - Import Ground
+"""
+
+import pygame
+import time
+
+pygame.init() # initialises pygame so it's ready to use
+
+llama = pygame.image.load("Llama.png")
+llama_resized = pygame.transform.scale(llama, (70, 70))
+ground = pygame.image.load("ground.png")
+ground_resized = pygame.transform.scale(ground, (1100, 550)) # changes width and height of the ground (to match the screen window)
+
+screen = pygame.display.set_mode((1100, 550)) # sets up the screen display
+
+def game_loop():
+    quit_game = False # variable name change from last version (stylistic purposes)
+
+    while not quit_game: # white quit game = false
+        # Variables v
+        screen.fill("White") # fills the screen background with white
+        screen.blit(llama_resized, (100, 355)) # displays the llama sprite at the set coordinates
+        screen.blit(ground_resized, (0,100))
+
+        # Executes if statements when certain events fire
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT: # when the user wants to clsoe the window
+                quit_game = True
+
+        pygame.display.update()
+
+game_loop()
+
+pygame.quit()
+quit()
